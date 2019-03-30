611631979b8b6f08fa99fef5f198f5e6fe162b48
austin vargason <vargasona@gmail.com>
2019-03-29 21:29:21 -0700

    added event gathering capability

--- Application/app/src/main/java/com/group5/atoms/CalendarFragment.java
+++ Application/app/src/main/java/com/group5/atoms/CalendarFragment.java
@@ -3,21 +3,25 @@ package com.group5.atoms;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.google.firebase.auth.FirebaseAuth;
-
 import java.util.ArrayList;
+
 
 
 /**
@@ -26,8 +30,9 @@ import java.util.ArrayList;
 public class CalendarFragment extends Fragment {
 
     //members needed
-    ArrayList<Long> calendarIds = new ArrayList<>();
-    String email;
+    private ArrayList<Long> calendarIds;
+    private ArrayList<String> events;
+    private String email;
 
     //TODO: text view for debugging, need to fill out actual recycler/card views
     TextView debugTextView;
@@ -59,8 +64,11 @@ public class CalendarFragment extends Fragment {
         //get the text view for debugging
         debugTextView = view.findViewById(R.id.debuggingTextView);
 
+        //initialize array lists
+        this.calendarIds = new ArrayList<>();
+        this.events = new ArrayList<>();
 
-        email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        this.email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
 
         if (email != null) {
             //create the calendarId's
@@ -73,36 +81,110 @@ public class CalendarFragment extends Fragment {
 
     //TODO: this needs to be implemented with a loader to prevent tying up the main thread
     private void setCalendarIds(String email) {
-            //get a cursor and the content resolver
-            Cursor cur = null;
-            ContentResolver cr = getActivity().getContentResolver();
+        //get a cursor and the content resolver
+        Cursor cur = null;
+        ContentResolver cr = getActivity().getContentResolver();
 
-            //declare mProjection
-            String[] mProjection =
-                    {
-                            CalendarContract.Calendars.ALLOWED_ATTENDEE_TYPES,
-                            CalendarContract.Calendars.ACCOUNT_NAME,
-                            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
-                            CalendarContract.Calendars.CALENDAR_LOCATION,
-                            CalendarContract.Calendars.CALENDAR_TIME_ZONE
-                    };
+        //declare mProjection
+        String[] mProjection =
+                {
+                        CalendarContract.Calendars.ALLOWED_ATTENDEE_TYPES,
+                        CalendarContract.Calendars.ACCOUNT_NAME,
+                        CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
+                        CalendarContract.Calendars.CALENDAR_LOCATION,
+                        CalendarContract.Calendars.CALENDAR_TIME_ZONE
+                };
 
-            //set the uri and the selection args using the user's email
-            Uri uri = CalendarContract.Calendars.CONTENT_URI;
-            String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
-                    + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
-            String[] selectionArgs = new String[]{email, email};
+        //set the uri and the selection args using the user's email
+        Uri uri = CalendarContract.Calendars.CONTENT_URI;
+        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
+                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
+        String[] selectionArgs = new String[]{email, email};
 
-            //run the query
-            cur = cr.query(uri, mProjection, selection, selectionArgs, null);
+        //run the query
+        cur = cr.query(uri, mProjection, selection, selectionArgs, null);
 
-            //get the data from the cursor
-            while (cur.moveToNext()) {
-                calendarIds.add(cur.getLong(PROJECTION_ID_INDEX));
-            }
+        //get the data from the cursor
+        while (cur.moveToNext()) {
+            this.calendarIds.add(cur.getLong(PROJECTION_ID_INDEX));
+        }
 
-            //update the UI
-            updateCalendarUI();
+        //get the event data
+        readEvents(2);
+
+        //update the UI
+        updateCalendarUI();
+    }
+
+    public void readEvents(int timeFrame) {
+        final String[] INSTANCE_PROJECTION = new String[]{
+                CalendarContract.Instances.EVENT_ID,      // 0
+                CalendarContract.Instances.BEGIN,         // 1
+                CalendarContract.Instances.TITLE,          // 2
+                CalendarContract.Instances.ORGANIZER
+        };
+
+        // The indices for the projection array above.
+        final int PROJECTION_ID_INDEX = 0;
+        final int PROJECTION_BEGIN_INDEX = 1;
+        final int PROJECTION_TITLE_INDEX = 2;
+        final int PROJECTION_ORGANIZER_INDEX = 3;
+
+        // Specify the date range you want to search for recurring event instances
+        Calendar calendar = Calendar.getInstance();
+        long startMillis;
+        long endMillis;
+        DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        int currentMonth = calendar.get(Calendar.MONTH);
+        int currentYear = calendar.get(Calendar.YEAR);
+
+        // Construct the query with the desired date range.
+        Uri.Builder builder = CalendarContract.Instances.CONTENT_URI.buildUpon();
+
+        switch (timeFrame) {
+            case 1:
+                calendar.set(currentYear, currentMonth, calendar.get(Calendar.DATE), 0, 0);
+                startMillis = calendar.getTimeInMillis();
+                int endMonth = calendar.getActualMaximum(Calendar.DATE);
+                calendar.set(currentYear, currentMonth, endMonth, 23, 59, 59);
+                endMillis = calendar.getTimeInMillis();
+                break;
+            case 2:
+                calendar.set(currentYear, 1, 1, 0, 0);
+                startMillis = calendar.getTimeInMillis();
+                calendar.set(currentYear, 12, 31, 23, 59, 59);
+                endMillis = calendar.getTimeInMillis();
+                break;
+            default:
+                startMillis = calendar.getTimeInMillis();
+                calendar.add(Calendar.DATE, 1);
+                endMillis = calendar.getTimeInMillis();
+                break;
+        }
+
+
+        // Submit the query
+        ContentUris.appendId(builder, startMillis);
+        ContentUris.appendId(builder, endMillis);
+        Cursor cur = getActivity().getContentResolver().query(builder.build(), INSTANCE_PROJECTION, null, null, null);
+
+        while (cur.moveToNext()) {
+
+            // Get the field values
+            long eventID = cur.getLong(PROJECTION_ID_INDEX);
+            long beginVal = cur.getLong(PROJECTION_BEGIN_INDEX);
+            String title = cur.getString(PROJECTION_TITLE_INDEX);
+            String organizer = cur.getString(PROJECTION_ORGANIZER_INDEX);
+
+            // Do something with the values.
+            Log.i("Calendar", "Event:  " + title);
+            calendar.setTimeInMillis(beginVal);
+
+            Log.i("Calendar", "Date: " + formatter.format(calendar.getTime()));
+
+            this.events.add(String.format("\nEvent: %s\nID: %s\nOrganizer: %s\nDate: %s\n", title, eventID + "", organizer, formatter.format(calendar.getTime())));
+        }
+
 
     }
 
@@ -116,7 +198,12 @@ public class CalendarFragment extends Fragment {
 
         //fill the string builder with the calendar IDs
         for (Long id: this.calendarIds) {
-            sb.append(id + "\n");
+            sb.append("CalendarID: " + id + "\n");
+        }
+
+        //fill the text view with the event data
+        for (String eventLine : this.events) {
+            sb.append(eventLine + "\n");
         }
 
         //update the textView
